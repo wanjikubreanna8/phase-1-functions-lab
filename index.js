f028676dfe0845c65ebb17fc628dc6e0aaef7886
breanna <breannawanjiku8@gmail.com>
2025-03-19 13:55:23 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(position) {
+    return Math.abs(position -42);
+}
+function distanceFromHqInFeet(position) {
+    return distanceFromHqInBlocks(position) * 264;
+}
+function distanceTravelledInFeet(depart, arrive) {
+    return Math.abs(arrive - depart) * 264;
+}
+function calculatesFarePrice( depart, arrive){
+    const distance = distanceTravelledInFeet(depart, arrive);
+
+    if ( distance <= 400) {
+        return 0;
+    }
+    else if (distance <= 2000){
+        return (distance -400) *0.02;
+    }
+    else if (distance <= 2500){
+        return 25;
+    }
+    else {
+        return "cannot travel that far"
+    }
+}
